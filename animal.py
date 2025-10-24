6841701b73f24429bc7d967a55205ea6ab962be9
Ayesha Siddiqa <siday032@mymail.unisa.edu.au>
2025-10-24 12:44:59 +0300

    Added Animal class skeleton with protected attributes and methods

--- animal.py
+++ animal.py
@@ -1,8 +1,27 @@
-'''
-File: filename.py
+"""
+File: animal.py
 Description: A brief description of this Python module.
-Author: Billy Bizilis
+Author: Ayesha Siddiqa
 ID: 110100110
-Username: bizvy001
+Username: SIDAY032
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+"""
+class Animal:
+    def __init__(self, name, species, age, dietary_needs):
+        self._name = name
+        self._species = species
+        self._age = age
+        self._dietary_needs = dietary_needs
+
+    def eat(self):
+        pass
+    def sleep(self):
+        pass
+    def make_sound(self):
+        pass
+    def __str__(self):
+        pass
+    def __eq__(self, other):
+        pass
+
+
